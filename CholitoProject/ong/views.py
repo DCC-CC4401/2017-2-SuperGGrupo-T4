1d5ee987d0298702fdf639c2b9b044589a683cf9
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-11-13 20:15:16 -0300

    Send context

--- CholitoProject/ong/views.py
+++ CholitoProject/ong/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 
 from CholitoProject.userManager import get_user_index
+from complaint.models import AnimalType
 from naturalUser.models import ONGLike
 from ong.models import ONG
 
 
 class ONGNaturalView(View):
     template_name = 'temp_like.html'
-    context = {}
+    context = {'animals': AnimalType.objects.all()}
 
     def get(self, request, pk, **kwargs):
         c_user = get_user_index(request.user)

27a02d5ea7f792c3f08ab51bea99327d4ba25871
Jorge Gutierrez <jgutierrezt2@hotmail.com>
2017-11-18 20:51:33 -0300

    added filter by favorites and not fav ong and added star image for fav ones

--- CholitoProject/naturalUser/models.py
+++ CholitoProject/naturalUser/models.py
@@ -20,7 +20,25 @@ class NaturalUser(models.Model):
         return "Natural user " + self.user.username
 
     def get_index(self, request, context=None):
+        liked, not_liked = self.filter_ongs(context['ongs'])
+        context['liked_ongs'] = liked
+        context['ongs'] = not_liked
         return render(request, 'index.html', context=context)
+
+    def filter_ongs(self, ongs):
+        liked_ongs = []
+        not_liked_ongs = []
+        for ong in ongs:
+            print(ong.lat)
+            if self.like_ong(ong):
+                liked_ongs.append(ong)
+            else :
+                not_liked_ongs.append(ong)
+        return (liked_ongs, not_liked_ongs)
+
+    def like_ong(self, ong):
+        return ONGLike.objects.all().filter(natural_user=self,ong=ong).count()
+
 
 
 class ONGLike(models.Model):

e70bccd4aff77454b43814e7102bdc1a8d1acf25
jpintoriv <jorgepintoriveros@gmail.com>
2017-11-18 04:21:05 -0300

    widgets in form

--- CholitoProject/animals/forms.py
+++ CholitoProject/animals/forms.py
@@ -4,7 +4,7 @@ from animals.models import Animal
 from complaint.models import AnimalType, Complaint
 
 
-class ComplaintForm(forms.ModelForm):
+class AnimalForm(forms.ModelForm):
     class Meta:
         model = Animal
         fields = (
@@ -14,13 +14,25 @@ class ComplaintForm(forms.ModelForm):
             'color',
             'estimated_age',
             'description',
-            'from_complaint',
-            'admission_date',
+            'adoption_state',
             'is_sterilized',
-
         )
+        widgets = {
+            'name' : forms.TextInput(attrs={'id': 'name-input'}),
+            'gender' : forms.RadioSelect(attrs={'id': 'gender-input'}),
+            'animal_type' : forms.Select(attrs={'id': 'animal_type-input'}),
+            'color': forms.TextInput(attrs={'id': 'color-input'}),
+            'estimated_age': forms.NumberInput(attrs={'id': 'estimated_age-input'}),
+            'description': forms.TextInput(attrs={'id': 'description-input'}),
+            'adoption_state': forms.HiddenInput(),
+            'is_sterilized': forms.Select(attrs={'id': 'is_sterilized-input'})
+        }
 
     def __init__(self, *args, **kwargs):
-        super(ComplaintForm, self).__init__(*args, **kwargs)
+        super(AnimalForm, self).__init__(*args, **kwargs)
         self.fields['animal_type'] = forms.ModelChoiceField(queryset=AnimalType.objects)
-        self.fields['from_complaint'] = forms.ModelChoiceField(queryset=Complaint.objects)
+
+class ImageForm(forms.Form):
+    animal_image = forms.ImageField(required=False,
+        widget=forms.FileInput(
+            attrs={'id': 'image-input', 'class': "form-control", 'placeholder': "Agrega Imagen"}))

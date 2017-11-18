cf8e230ebe3b78e1d6b6829db2f654c70feccd23
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-11-18 17:11:48 -0300

    Fix animal avatar not showing

--- CholitoProject/animals/models.py
+++ CholitoProject/animals/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+
 from complaint.models import Complaint, AnimalType
 from naturalUser.models import NaturalUser
 from ong.models import ONG
+
 
 def default_avatar():
     return 'animals/animal-default.jpg'
@@ -29,19 +31,19 @@ class Animal(models.Model):
         (False, "No")
     )
 
-
-
     name = models.TextField(max_length=100)
     gender = models.SmallIntegerField(choices=GENDER_OPTIONS)
     description = models.TextField(max_length=1000)
     animal_type = models.ForeignKey(AnimalType)
     color = models.TextField(max_length=50)
     estimated_age = models.PositiveSmallIntegerField()
-    admission_date = models.DateTimeField(auto_now_add = True)
-    adoption_state = models.SmallIntegerField(choices=ADOPTION_OPTIONS, blank=1, null=1)
-    is_sterilized = models.NullBooleanField(choices=STERILIZED_OPTIONS, blank=False, null=False)
+    admission_date = models.DateTimeField(auto_now_add=True)
+    adoption_state = models.SmallIntegerField(choices=ADOPTION_OPTIONS,
+                                              blank=1, null=1)
+    is_sterilized = models.NullBooleanField(choices=STERILIZED_OPTIONS,
+                                            blank=False, null=False)
     ong = models.ForeignKey(ONG, on_delete=models.CASCADE)
-    avatar = models.ImageField(default = default_avatar, upload_to="animals/")
+    avatar = models.ImageField(default=default_avatar, upload_to="animals/")
 
     def __str__(self):
         return self.name + " - " + self.animal_type.name

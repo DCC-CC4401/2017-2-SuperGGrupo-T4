4cbd370c67103ade12f1e014b74a50167533f4f6
Juan-Pablo Silva <jpsilvapena@hotmail.com>
2017-11-20 17:15:36 -0300

    Remove conflicting trailing code

--- CholitoProject/ong/urls.py
+++ CholitoProject/ong/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
 
-from ong.views import ONGDispatcherView, ONGIndexView, ONGAdoptedView, \
-    ONGStatisticsView, ONGEditView, ONGAddAnimalView, ONGRequestsView, \
-    ONGFavView, ONGEditAnimalView, ONGCreateAnimalView, \
-    ONGEUpdateAnimalView, ONGAnimalView
+from ong.views import ONGDispatcherView, ONGIndexView, ONGStatisticsView, \
+    ONGEditView, ONGAddAnimalView, ONGRequestsView, ONGFavView, \
+    ONGEditAnimalView, ONGCreateAnimalView, ONGEUpdateAnimalView, ONGAnimalView
 
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', ONGDispatcherView.as_view(), name='see-natural-ong'),
+    url(r'^(?P<pk>\d+)/$', ONGDispatcherView.as_view(),
+        name='see-natural-ong'),
     url(r'^$', ONGIndexView.as_view(), name='ong-index'),
-    url(r'^adopted/$', ONGAdoptedView.as_view(), name='ong-adopted'),
     url(r'^statistics/$', ONGStatisticsView.as_view(), name='ong-statistics'),
     url(r'^edit/$', ONGEditView.as_view(), name='ong-edit'),
     url(r'^add/$', ONGAddAnimalView.as_view(), name='add-animal'),
